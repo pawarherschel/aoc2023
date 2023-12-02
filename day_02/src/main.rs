66646c418f1b0f9ac8b1b7b18847b71daba67591
pawarherschel <pawarherschel@gmail.com>
2023-12-02 13:24:17 +0530

    benchmark mode

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,4 +1,5 @@
-#![allow(unused)]
+use std::time::{Duration, Instant};
+
 fn main() {
     // i: input
     // e: example
@@ -6,31 +7,38 @@ fn main() {
     // a: answer
     // c: calculated
 
-    let i1 = include_str!("i1.txt");
-    let e1 = include_str!("e1.txt");
-    let ga1 = "8";
-    let ga1 = ga1.to_string();
+    let start = Instant::now();
 
-    let i2 = include_str!("i2.txt");
-    let e2 = include_str!("e2.txt");
-    let ga2 = "2286";
-    let ga2 = ga2.to_string();
+    for _ in 0..10_000 {
+        let i1 = include_str!("i1.txt");
+        let e1 = include_str!("e1.txt");
+        let ga1 = "8";
+        let ga1 = ga1.to_string();
 
-    let ea1 = part1(e1);
-    assert_eq!(ga1, ea1);
-    let a1 = part1(i1);
-    println!("--- PART1: {a1}");
-    let ca1 = "2331";
-    let ca1 = ca1.to_string();
-    assert_eq!(ca1, a1);
+        let i2 = include_str!("i2.txt");
+        let e2 = include_str!("e2.txt");
+        let ga2 = "2286";
+        let ga2 = ga2.to_string();
 
-    let ea2 = part2(e2);
-    assert_eq!(ga2, ea2);
-    let a2 = part2(i2);
-    println!("--- PART2: {a2}");
-    let ca2 = "71585";
-    let ca2 = ca2.to_string();
-    assert_eq!(ca2, a2);
+        let ea1 = part1(e1);
+        assert_eq!(ga1, ea1);
+        let a1 = part1(i1);
+        println!("--- PART1: {a1}");
+        let ca1 = "2331";
+        let ca1 = ca1.to_string();
+        assert_eq!(ca1, a1);
+
+        let ea2 = part2(e2);
+        assert_eq!(ga2, ea2);
+        let a2 = part2(i2);
+        println!("--- PART2: {a2}");
+        let ca2 = "71585";
+        let ca2 = ca2.to_string();
+        assert_eq!(ca2, a2);
+    }
+
+    let time = Duration::from_secs_f64(start.elapsed().as_secs_f64() / 10_000.0);
+    dbg!(time);
 }
 
 #[derive(Clone, Default, Debug)]
@@ -54,8 +62,7 @@ static EXAMPLE_01_FILTER: DieFin = DieFin {
 };
 
 fn part1(input: &str) -> String {
-    let mut state = vec![];
-    for line in input.lines() {
+    let state = input.lines().map(|line| {
         let (id_raw, subsets_raw) = line.split_once(':').unwrap();
         let id = id_raw.split_once(' ').unwrap().1.parse::<u32>().unwrap();
 
@@ -84,8 +91,8 @@ fn part1(input: &str) -> String {
                 }
             }
         }
-        state.push((id, game));
-    }
+        (id, game)
+    });
     state
         .into_iter()
         .filter(|(_, die)| {
@@ -95,14 +102,12 @@ fn part1(input: &str) -> String {
                 || blue.iter().any(|it| it > &EXAMPLE_01_FILTER.blue))
         })
         .map(|(it, _)| it)
-        .map(|it| it)
         .sum::<u32>()
         .to_string()
 }
 
 fn part2(input: &str) -> String {
-    let mut state = vec![];
-    for line in input.lines() {
+    let state = input.lines().map(|line| {
         let (id_raw, subsets_raw) = line.split_once(':').unwrap();
         let id = id_raw.split_once(' ').unwrap().1.parse::<u32>().unwrap();
 
@@ -131,11 +136,12 @@ fn part2(input: &str) -> String {
                 }
             }
         }
-        state.push((id, game));
-    }
+        (id, game)
+    });
+
     state
         .into_iter()
-        .map(|(id, dies)| {
+        .map(|(_, dies)| {
             let Die { red, green, blue } = dies;
 
             let red = red.into_iter().max().unwrap();
